435553f7d5092e76034286bda83f8531c50bfa9a
saranshg04 <30841336+saranshg04@users.noreply.github.com>
2017-12-26 16:43:34 +0530

    change

--- public/models/mobile_fact/mobFact.js
+++ public/models/mobile_fact/mobFact.js
@@ -2,7 +2,7 @@ app.factory("mobFactory", function ($http, $q) {
         var getJSON=function () {
         var defered = $q.defer();
         console.log("Calling JSON");
-        $http.get("http://localhost:8080/mobiles").then(function (data) {
+        $http.get("https://saransh-ecommerce.herokuapp.com/mobiles").then(function (data) {
             console.log("Inside success"+data.data);
             defered.resolve(data);
         }, function (er) {
