48b95969944c7fb2ece026b537a8e4b17c1cff88
saranshg04 <30841336+saranshg04@users.noreply.github.com>
2017-12-26 17:15:26 +0530

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //web: node index.js
 const express= require("express");
 const app=express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.use(express.static("public"));
 
 app.use(function(req, res, next) {
